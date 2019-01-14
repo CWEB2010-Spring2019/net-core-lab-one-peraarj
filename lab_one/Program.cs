92f0255bfb8fd453ec662ed3970751c50f713f32
Aaron Perry <peraarj@dunwoody.edu>
2019-01-14 11:59:28 -0600

    adding a commit in the mergetest branch

--- lab_one/Program.cs
+++ lab_one/Program.cs
@@ -9,6 +9,7 @@ namespace lab_one
             Console.WriteLine("Hello World!");
 			Console.WriteLine("This is my first change to the file for test commits");
 			Console.WriteLine("This is my second change to the file for test commits");
+			Console.WriteLine("Adding a test in a different branch to test out version controll");
         }
     }
 }
