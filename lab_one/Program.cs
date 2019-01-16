f2d92fcafb0989a1b5761b146e390c633307e201
Aaron Perry <peraarj@dunwoody.edu>
2019-01-16 12:49:23 -0600

    Created some variables and workout some syntax

--- lab_one/Program.cs
+++ lab_one/Program.cs
@@ -2,14 +2,30 @@
 
 namespace lab_one
 {
+	// This program is a quiz application that will ask answeres and have a multiple choice option
+	// The use of mulitple arrays will be used to store the questions and answers
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-			Console.WriteLine("This is my first change to the file for test commits");
-			Console.WriteLine("This is my second change to the file for test commits");
-			Console.WriteLine("Adding a test in a different branch to test out version controll");
-        }
+			// Answer key for quiz app is "B", "B", "c", "A", "D", "A", "A", "C", "D", "A"
+			string[] correctAnswers = new string[10] {"B", "B", "c", "A", "D", "A", "A", "C", "D", "A"};
+			string[] userSelection = new string[10];
+			string[,] quizQuestionOptions = {{"A)", "B)", "C)", "D)"},
+											 {"A)", "B)", "C)", "D)"},
+											 {"A)", "B)", "C)", "D)"},
+											 {"A)", "B)", "C)", "D)"},
+											 {"A)", "B)", "C)", "D)"},
+											 {"A)", "B)", "C)", "D)"},
+											 {"A)", "B)", "C)", "D)"},
+											 {"A)", "B)", "C)", "D)"},
+											 {"A)", "B)", "C)", "D)"},
+											 {"A)", "B)", "C)", "D)"}};
+			string selection;
+			int start;
+			int EXIT;
+
+
+		}
     }
 }
